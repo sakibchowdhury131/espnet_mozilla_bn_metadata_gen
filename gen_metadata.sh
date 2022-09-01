1d2d0a4e2429560a5a3ef27009e696c3b30febcd
Your Name <you@example.com>
2022-09-01 14:38:59 +0600

    server tested

--- gen_metadata.sh
+++ gen_metadata.sh
@@ -1,21 +1,21 @@
 # define paths
-TRAIN=./train.tsv
-DEV=./dev.tsv
-TEST=./test.tsv
-OTHER=./other.tsv
-INVALIDATED=./invalidated.tsv
-VALIDATED=./validated.tsv
+TRAIN=../../train.tsv
+DEV=../../dev.tsv
+TEST=../../test.tsv
+OTHER=../../other.tsv
+INVALIDATED=../../invalidated.tsv
+VALIDATED=../../validated.tsv
 
-
-AUDIO_DESTINATION=./audio_destination
+AUDIO_SOURCE=../../clips_in_flac
+AUDIO_DESTINATION=../../mozilla_flac_metadata_compat
 rm -rf $AUDIO_DESTINATION
 mkdir $AUDIO_DESTINATION
 
 #generate new metadata
-METADATA_OUT=./mozilla_metadata.txt
+METADATA_OUT=../../mozilla_metadata.txt
 echo '' > $METADATA_OUT
 
-list_of_audios=(`ls audio-files`)
+list_of_audios=(`ls $AUDIO_SOURCE`)
 for i in "${list_of_audios[@]}"
 do
     :
@@ -44,39 +44,39 @@ do
                         then
                             echo "$UTTID has no log"
                         else
-                            cp audio-files/$i $AUDIO_DESTINATION    # copy the audio file
+                            cp $AUDIO_SOURCE/$i $AUDIO_DESTINATION    # copy the audio file
                             TEXT=(`cat $VALIDATED | grep $UTTID | cut -f3`)
-                            DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 audio-files/$i 2>/dev/null`)
+                            DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $AUDIO_SOURCE/$i 2>/dev/null`)
                             echo $UTTID$'\t'$SPKID$'\t'${TEXT[*]}$'\t'$DURATION >> $METADATA_OUT
                         fi
                     else
-                        cp audio-files/$i $AUDIO_DESTINATION    # copy the audio file
+                        cp $AUDIO_SOURCE/$i $AUDIO_DESTINATION    # copy the audio file
                         TEXT=(`cat $INVALIDATED | grep $UTTID | cut -f3`)
-                        DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 audio-files/$i 2>/dev/null`)
+                        DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $AUDIO_SOURCE/$i 2>/dev/null`)
                         echo $UTTID$'\t'$SPKID$'\t'${TEXT[*]}$'\t'$DURATION >> $METADATA_OUT
                     fi
                 else
-                    cp audio-files/$i $AUDIO_DESTINATION    # copy the audio file
+                    cp $AUDIO_SOURCE/$i $AUDIO_DESTINATION    # copy the audio file
                     TEXT=(`cat $DEV | grep $UTTID | cut -f3`)
-                    DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 audio-files/$i 2>/dev/null`)
+                    DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $AUDIO_SOURCE/$i 2>/dev/null`)
                     echo $UTTID$'\t'$SPKID$'\t'${TEXT[*]}$'\t'$DURATION >> $METADATA_OUT
                 fi
             else
-                cp audio-files/$i $AUDIO_DESTINATION    # copy the audio file
+                cp $AUDIO_SOURCE/$i $AUDIO_DESTINATION    # copy the audio file
                 TEXT=(`cat $TEST | grep $UTTID | cut -f3`)
-                DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 audio-files/$i 2>/dev/null`)
+                DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $AUDIO_SOURCE/$i 2>/dev/null`)
                 echo $UTTID$'\t'$SPKID$'\t'${TEXT[*]}$'\t'$DURATION >> $METADATA_OUT
             fi
         else
-            cp audio-files/$i $AUDIO_DESTINATION    # copy the audio file
+            cp $AUDIO_SOURCE/$i $AUDIO_DESTINATION    # copy the audio file
             TEXT=(`cat $TRAIN | grep $UTTID | cut -f3`)
-            DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 audio-files/$i 2>/dev/null`)
+            DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $AUDIO_SOURCE/$i 2>/dev/null`)
             echo $UTTID$'\t'$SPKID$'\t'${TEXT[*]}$'\t'$DURATION >> $METADATA_OUT
         fi
     else
-        cp audio-files/$i $AUDIO_DESTINATION    # copy the audio file
+        cp $AUDIO_SOURCE/$i $AUDIO_DESTINATION    # copy the audio file
         TEXT=(`cat $OTHER | grep $UTTID | cut -f3`)
-        DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 audio-files/$i 2>/dev/null`)
+        DURATION=(`ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $AUDIO_SOURCE/$i 2>/dev/null`)
         echo $UTTID$'\t'$SPKID$'\t'${TEXT[*]}$'\t'$DURATION >> $METADATA_OUT
     fi
 done
